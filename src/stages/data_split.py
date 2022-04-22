e0155505342a855df9b55fcc280001ea5e8d5f80
choisuren m <choisuren.m@gcm.mn>
2022-04-22 12:17:50 +0800

    Intial push

--- src/stages/data_split.py
+++ src/stages/data_split.py
@@ -11,10 +11,15 @@ def data_split(config_path: Text)->None:
     with open ("reports/params.yaml") as conf_file:
         config=yaml.safe_load(conf_file)
     
-    dataset=pd.read_csv(config['featurize']['features_path'])
-    train_dataset, test_dataset = train_test_split(dataset, test_size=config["data_split"]['test_size'], random_state=42)
+    dataset=pd.read_csv(config['featurize']['dataset_csv'])
+    train_dataset, test_dataset = train_test_split(dataset, test_size=config["split_train_test"]['test_size'], random_state=42)
     train_path=config["data"]["trainset_path"]
     test_path=config["data"]["testset_path"]
     train_dataset.to_csv(train_path,index=False)
     test_dataset.to_csv(test_path, index=False)
-    
+    print("Data split complete")
+if __name__ == '__main__':
+    args_parser=argparse.ArgumentParser()
+    args_parser.add_argument('--config',dest='config',required=True)
+    args=args_parser.parse_args()
+    data_split(config_path=args.config)
